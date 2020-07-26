4d137d308ca9e70e25ba95d12f395033e6550ca4
Marco Hung <37731008+marcohung818@users.noreply.github.com>
2020-07-27 02:03:33 +0800

    Ui swiper

---
+++ Chinese Battle/Assets/Script/UI/UISwiper.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UISwiper : MonoBehaviour, IDragHandler, IEndDragHandler
+{
+    Vector2 panelLocation;
+    public float percentThrehold = 0.2f;
+    public float easing = 0.5f;
+    public void OnDrag(PointerEventData eventData)
+    {
+        float posDiff = eventData.pressPosition.x - eventData.position.x;
+        transform.position = panelLocation - new Vector2(posDiff, 0);
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        float percentage = (eventData.pressPosition.x - eventData.position.x) / Screen.width;
+        if(Mathf.Abs(percentage) >= percentThrehold)
+        {
+            Vector2 newLocation = panelLocation;
+            if(percentage > 0)
+            {
+                newLocation += new Vector2(-Screen.width, 0);
+            }
+            else if(percentage < 0)
+            {
+                newLocation += new Vector2(Screen.width, 0);
+            }
+            StartCoroutine(SmoothMove(transform.position, newLocation, easing));
+            panelLocation = newLocation;
+        }
+        else
+        {
+            StartCoroutine(SmoothMove(transform.position, panelLocation, easing));
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        panelLocation = transform.position;
+    }
+
+    IEnumerator SmoothMove(Vector2 startPos, Vector2 endPos, float seconds)
+    {
+        float t = 0f;
+        while(t <= 1.0f)
+        {
+            t += Time.deltaTime / seconds;
+            transform.position = Vector2.Lerp(startPos, endPos, Mathf.SmoothStep(0f, 1f, t));
+            yield return null;
+        }
+    }
+
+}
