e0a9789c19b57df2dc7ba58d5373c7d6d226967e
Marco Hung <37731008+marcohung818@users.noreply.github.com>
2020-08-11 01:56:05 +0800

    UI  setting

---
+++ Chinese Battle/Assets/Script/UI/EqualPanel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EqualPanel : MonoBehaviour
+{
+    [SerializeField] private GameObject wordEquipPanel;
+    [SerializeField] private GameObject characterEquipPanel;
+
+    public void EnableCharacterEquipPanel()
+    {
+        wordEquipPanel.SetActive(false);
+        characterEquipPanel.SetActive(true);
+    }
+
+    public void EnableWordEquipPanel()
+    {
+        wordEquipPanel.SetActive(true);
+        characterEquipPanel.SetActive(false);
+    }
+}
